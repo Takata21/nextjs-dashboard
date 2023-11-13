b4dc4a3460401c5a0f15a7e6e36d11e0c9f01aa0
Manuel Takata <takata211011@gmail.com>
2023-11-12 23:12:40 -0500

    starting chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -56,6 +57,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
